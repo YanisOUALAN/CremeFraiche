aaf18504cd1b2d5186edcd0dddbfcf4c9143732a
Darck Sensei <oualanyanis01@gmail.com>
2022-01-09 19:41:03 +0100

    Initial Commit

---
+++ CremeFraiche/CremeFraicheApp.swift
@@ -0,0 +1,17 @@
+//
+//  CremeFraicheApp.swift
+//  CremeFraiche
+//
+//  Created by Yanis OUALAN on 09/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CremeFraicheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
